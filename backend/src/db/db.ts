7cff02b330637c09ec52250acdd2f2d6f24a04d6
Urip Subekti <urip.subekti13@gmail.com>
2023-11-24 14:48:34 +0700

    feat: use json  as db

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -1,3 +1,14 @@
+import {
+  PathOrFileDescriptor,
+  existsSync,
+  mkdirSync,
+  readFileSync,
+  writeFileSync,
+} from "fs";
+import path from "path";
+const dataDir = path.join(process.cwd(), "data");
+const filePathCfg = path.join(dataDir, "config.json");
+
 export type IData = {
   user: {
     name: string;
@@ -21,93 +32,39 @@ export type IBookmarkItem = {
   url: string;
   icon: string;
 };
+
+const initValue = {
+  user: {
+    name: "Admin",
+    email: "admin@admin.com",
+    password: "admin",
+    avatar: "male_24.png",
+  },
+  bookmarks: [],
+};
+
 class Database {
-  private _data: IData = {
-    user: {
-      name: "Urip Subekti",
-      email: "admin@admin.com",
-      password: "admin",
-      avatar: "male_24.png",
-    },
+  private _data: IData = initValue;
 
-    bookmarks: [
-      {
-        id: 1,
-        name: "Apps",
-        sort: 1,
-        items: [
-          {
-            id: 1,
-            name: "Vaultwarden",
-            url: "https://bw.uripsub.dev/",
-            sort: 1,
-            icon: "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/svg/bitwarden.svg",
-          },
-          {
-            id: 2,
-            name: "Adguard",
-            url: "https://adguard.uripsub.dev/",
-            sort: 2,
-            icon: "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/svg/adguard-home.svg",
-          },
-          {
-            id: 3,
-            name: "Daily Expense",
-            url: "https://dew.uripsub.dev/",
-            sort: 3,
-            icon: "https://dew.uripsub.dev/icon.svg",
-          },
-          {
-            id: 4,
-            name: "Blog",
-            url: "https://blog.uripsub.dev/",
-            sort: 4,
-            icon: "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/png/ghost-light.png",
-          },
-          {
-            id: 5,
-            name: "Darwin InOut",
-            url: "https://darwin1309.uripsub.dev/",
-            sort: 5,
-            icon: "https://cdn-images-1.medium.com/v2/resize:fit:1200/1*7QHmE3Q4EW_fSqpGOpcyrg.png",
-          },
-          {
-            id: 6,
-            name: "uBrain",
-            url: "https://ubrain.uripsub.dev/",
-            sort: 6,
-            icon: "https://www.svgrepo.com/download/353662/docusaurus.svg",
-          },
-          {
-            id: 7,
-            name: "Ghea",
-            url: "https://ghea.uripsub.dev/_/",
-            sort: 7,
-            icon: "https://seeklogo.com/images/P/pocketbase-logo-CA73994F09-seeklogo.com.png",
-          },
-          {
-            id: 8,
-            name: "Droppy",
-            url: "https://droppy.uripsub.dev/",
-            sort: 8,
-            icon: "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/svg/droppy.svg",
-          },
-          {
-            id: 9,
-            name: "Code",
-            url: "https://code.uripsub.dev/",
-            sort: 9,
-            icon: "https://cdn.jsdelivr.net/gh/walkxcode/dashboard-icons/svg/vscode.svg",
-          },
-        ],
-      },
-    ],
-  };
+  private writeToFile(file: PathOrFileDescriptor, data: any) {
+    writeFileSync(file, JSON.stringify(data, null, 3));
+  }
 
-  public init() {}
+  private loadFromFileCfg(file: PathOrFileDescriptor, defaultValue: any) {
+    return JSON.parse(readFileSync(file, "utf-8") || defaultValue);
+  }
+
+  public init() {
+    if (existsSync(filePathCfg)) {
+      this._data = this.loadFromFileCfg(filePathCfg, initValue);
+    } else {
+      if (!existsSync(dataDir)) mkdirSync(dataDir, { recursive: true });
+      this.writeToFile(filePathCfg, this._data);
+    }
+  }
 
   public getUser() {
-    return this._data.user;
+    return this._data?.user;
   }
 
   public getBookmarks() {
